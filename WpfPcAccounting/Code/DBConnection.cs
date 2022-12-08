b661eefcc92f043daba476d73c8e1734dcab7c32
Арсений Фролов <tincofa@yandex.ru>
2022-12-08 16:07:32 +0300

    Добавьте файлы проекта.

---
+++ WpfPcAccounting/Code/DBConnection.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfPcAccounting.Code
+{
+    static class DBConnection
+    {
+        public static AccountingOfEquipmentDB DB = new AccountingOfEquipmentDB();
+    }
+}
